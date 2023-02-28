3c4e5f9e98f9d2de513584b7f0aa895977ee9be8
Yauhen Lis <jfox1389@yandex.ru>
2023-02-28 13:56:43 +0300

    прикручиваем тг_бота

---
+++ config.py
@@ -0,0 +1,25 @@
+bot_token = "6055698659:AAEPnST5AWCvjz1LRt8pEbHAnhoVfscWaQ8"
+tg_admin_id = 809854135
+
+
+fake_database = {'users':[
+    {
+        "id":1,
+        "name":"Anna",
+        "nick":"Anny42",
+        "balance": 15300
+     },
+
+    {
+        "id":2,
+        "name":"Dima",
+        "nick":"dimon2319",
+        "balance": 160.23
+     },
+    {
+        "id":3,
+        "name":"Vladimir",
+        "nick":"Vova777",
+        "balance": 200.1
+     }
+    ], }
